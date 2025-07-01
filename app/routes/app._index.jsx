bcde0fff0c1c3fc72d2835b1af8ebe759bec0a83
Vahid Reyazat <vreyazat@gmail.com>
2025-07-01 18:03:26 +0330

    debug

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -1,36 +1,18 @@
 import { json } from "@remix-run/node";
-import { useLoaderData, Link, useNavigate } from "@remix-run/react";
 import { authenticate } from "../shopify.server";
 import {
-  Card,
-  EmptyState,
-  Layout,
-  Page,
-  IndexTable,
-  Thumbnail,
-  Text,
-  Icon,
-  InlineStack,
+   Page
 } from "@shopify/polaris";
-
-import { getQRCodes } from "../models/QRCode.server";
-import { AlertDiamondIcon, ImageIcon } from "@shopify/polaris-icons";
 
 // [START loader]
 export async function loader({ request }) {
   const { admin, session } = await authenticate.admin(request);
-  const qrCodes = await getQRCodes(session.shop, admin.graphql);
 
-  return json({
-    qrCodes,
-  });
+  return json({});
 }
 
 
 export default function Index() {
-  const { qrCodes } = useLoaderData();
-  const navigate = useNavigate();
-
   // [START page]
   return (
     <Page>
